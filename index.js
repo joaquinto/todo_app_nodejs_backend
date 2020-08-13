e1ba96a13749c632d7842f96ba175ff0e91bc85a
praizoh <oremei.akande@gmail.com>
2020-08-13 19:03:16 +0100

    #Design Application Struture (#1)
    * feature(structure):add app structure
    
    * feature(structure):add app structure
    
    * Delete .env
    
    Co-authored-by: Jonathan Odjegba <roejoeodj12@gmail.com>

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const cors = require("cors")
+const mongoose = require('mongoose')
+const path = require('path')
+app.use(cors());
+app.use(bodyparser.json())
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Todo app up and running' })
+});
+
+const port = process.env.PORT || 8085       
+app.listen(port, ()=> console.log(`listening on port ${port}...`))
+
+const dotenv=require('dotenv');
+
+dotenv.config();
+const mongoUrl = process.env.mongoUrl
+mongoose.connect(`${mongoUrl}`, { useNewUrlParser: true })
+.then(() => {  
+    console.log('Successfully connected to MongoDB Atlas!') 
+}) 
+.catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!')
+    console.error(error)
+});
+
+require('./app/router/todo.route')(app)
